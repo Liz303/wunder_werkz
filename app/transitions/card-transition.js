dd14e790944ff2bec0f769ecd1661671899c64ee
Liz Henderson <lhenderson303@gmail.com>
2015-09-22 10:31:22 -0600

    transitioning

--- app/transitions/card-transition.js
+++ app/transitions/card-transition.js
@@ -4,8 +4,14 @@ export default function fade(opts={}) {
   var firstStep;
   var secondStep;
   var thirdStep;
+  var fourthStep;
+  var fifthStep;
+  var sixthStep;
   var outOpts = opts;
   var frontCardElement = $(document.getElementById('front'));
+  var backCard = $(document.getElementById('back'));
+  var cardTwo = $(document.getElementById('card-two'));
+  var cardOne = $(document.getElementById('card-one'));
   var fadingElement = findFadingElement(this);
   if (fadingElement) {
     // We still have some older version that is in the process of
@@ -22,27 +28,31 @@ export default function fade(opts={}) {
     this.newElement.css('z-index', '0');
     this.newElement.find('#back').css('border', '3px solid white');
 
-    firstStep = animate(this.newElement, {left: ["-10px", "0px"], top: ["-10px", "0px"], opacity: [(opts.maxOpacity || 1), 0]}, {duration: 1000});
-    secondStep = animate(this.newElement, {opacity: [(opts.maxOpacity || 1), 0]}, {duration: 1000}, 'fade-in');
-    // thirdStep = animate(this.find('.back-card'))
-    thirdStep = animate(frontCardElement, {left: ["100%", "10px"]}, {duration: 1000});
+    // firstStep = animate(this.newElement, {left: ["-10px", "0px"], top: ["-10px", "0px"]}, {duration: 1000});
+    // secondStep = animate(this.newElement, {opacity: [(opts.maxOpacity || 1), 0]}, {duration: 1000}, 'fade-in');
+    // thirdStep = animate(backCard, {opacity: [(opts.minOpacity || 0), 1]}, {duration: 1000}, 'fade-out');
+    // fourthStep = animate(frontCardElement, {left: ["100%", "10px"]}, {duration: 1000});
+    // fifthStep = animate(this.newElement, {left: ["0px", "-10px"], top: ["0px", "-10px"]}, {duration: 1000});
     //secondStep = animate(this.oldElement, {opacity: 0}, outOpts, 'fade-out');
+    firstStep = animate(this.newElement, {left: ["-10px", "0px"], top: ["-10px", "0px"], opacity: [(opts.maxOpacity || 1), 0]}, {duration: 1000});
+    // secondStep = animate(this.newElement, {opacity: [(opts.maxOpacity || 1), 0]}, {duration: 1000}, 'fade-in');
+    thirdStep =  animate(frontCardElement, {left: ["100%", "10px"]}, {duration: 1000});
+    fourthStep = animate(this.newElement, {left: ["0px", "-10px"], top: ["0px", "-10px"]}, {duration: 1000});
   }
 
-   // return firstStep.then(() => {
-   //  return secondStep.then(() => {
-   //    return thirdStep.then(() => {
-   //      return
-   //    });
-   //  });
-   // });
-
   return firstStep.then(() => {
-    return secondStep.then(() => {
+    $(document.getElementById('back')).css('opacity', 0);
+    $(document.getElementById('card-one')).css('opacity', 0);
+    $(document.getElementById('card-two')).css('opacity', 0);
+    // return secondStep.then(() => {
       return thirdStep.then(() => {
-        return
-      });
-    });
+        return fourthStep.then(() => {
+            return
+          });
+            
+     
+       });
+    // });
    });
   // if (this.oldElement.find('.front-card').is(':animated')) {
 
